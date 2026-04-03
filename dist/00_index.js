c659744d9a899cbb0fc2fd0ab815892630c81dd4
mohammedrazaali-1 <mohammedrazaofficial@gmail.com>
2026-04-03 19:00:39 +0530

    my first hello program of typescript

---
+++ dist/00_index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let name = "mohammad raza";
+let age = 21;
+console.log(`hello my name is ${name} and my age is ${age} `);
---
+++ src/00_index.ts
@@ -0,0 +1,4 @@
+let name : string = "mohammad raza"
+let age: number = 21
+
+console.log(`hello my name is ${name} and my age is ${age} `)
